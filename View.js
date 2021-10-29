cb43480a13f2e12cbc0516db8e7e1bf9770effbe
JoakimHa <joakimha@getacademy.no>
2021-10-29 13:09:52 +0200

    b

--- View.js
+++ View.js
@@ -1,7 +1,7 @@
 
 View()
 function View() {
-    contentHomeDiv = `${Navbar} 
+    contentHomeDiv.innerHTML = `${Navbar} 
     <h1 class="main-text">Hub Home</h1>
     <p class="main-text">velkommen til HUB-en.</p> 
     <p class="main-text">Dette er en nettside med oversikt over alle oppgavene jeg og teamet mitt har gjort mellom 3 moduler på GET academy.</p> 
